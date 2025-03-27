d168c459e4c4f7477a02d458d8d4f55fc25e362f
reyesginop04 <reyesginop04@gmail.com>
2025-03-27 19:14:41 +0800

    fix: update option value from 'percentage' to 'percent' in ProductCostForm

--- src/pages/ProductCalculation.tsx
+++ src/pages/ProductCalculation.tsx
@@ -69,7 +69,7 @@ const ProductCostForm = () => {
           onChange={handleChange}
           className="border border-gray-300 rounded px-3 py-2 w-full"
         >
-          <option value="percentage">Percentage</option>
+          <option value="percent">Percentage</option>
           <option value="fixed">Fixed</option>
         </select>
       </div>
